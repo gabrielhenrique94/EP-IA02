6043e471f6f9154406560dc9be735a0c419081c4
Heloisa Carbone <heloisacarbone@gmail.com>
2015-06-15 18:18:52 -0300

    distancia cidades

--- AG-CaixeiroViajante/src/ProcessaCidades.java
+++ AG-CaixeiroViajante/src/ProcessaCidades.java
@@ -11,11 +11,15 @@ public class ProcessaCidades {
 	 */
 	public static int calculaPercurso(HashMap<Integer, int[]> posicaoCidades, ArrayList<Integer> sequenciaCidade) {
 		int distanciaPercorrida = 0;
+		int cidadeAtual = sequenciaCidade.get(0);
 		
-		/*
-		 * Fazer calculo
-		 */
-		
+		for (int i = 1; i < sequenciaCidade.size(); i++) {
+			int cidadeAux = sequenciaCidade.get(i);
+			distanciaPercorrida += (int) Math.ceil(Math.sqrt(
+					(posicaoCidades.get(cidadeAtual)[0] - posicaoCidades.get(cidadeAux)[0]) + 
+					(posicaoCidades.get(cidadeAtual)[1] - posicaoCidades.get(cidadeAux)[1])));
+			cidadeAtual = cidadeAux;
+		}
 		
 		return distanciaPercorrida;
 	}
