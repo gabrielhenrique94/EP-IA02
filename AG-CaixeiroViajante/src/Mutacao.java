37dd8b3b165f88d975e4b2fc8e158a3130a6e2bb
annabreganholi <annabreganholi@hotmail.com>
2015-06-26 15:52:33 -0300

    mutacao de posição implementada :)

--- AG-CaixeiroViajante/src/Mutacao.java
+++ AG-CaixeiroViajante/src/Mutacao.java
@@ -4,23 +4,26 @@ import java.util.HashMap;
 public class Mutacao {
 	/**
 	 * Metodo de mutacao de genes, utilizando mutacao inversiva.
-	 * @param filho - cromossomo que sofrera mutacao
+	 * 
+	 * @param filho
+	 *            - cromossomo que sofrera mutacao
 	 * @param mapaCidades
 	 * @return
 	 */
-	public static Cromossomo mutacaoInversiva(Cromossomo filho, HashMap<Integer, double[]> mapaCidades) {
-		
+	public static Cromossomo mutacaoInversiva(Cromossomo filho,
+			HashMap<Integer, double[]> mapaCidades) {
+
 		ArrayList<Integer> seqAtualFilho = filho.getGenotipo();
 		ArrayList<Integer> seqNovaFilho = new ArrayList<Integer>();
 		int aleatorio1 = -1;
 		int aleatorio2 = -1;
-		
-		while (aleatorio1 == aleatorio2 ) {
-			aleatorio1 = Helpers.intAleatorio(0, seqAtualFilho.size()-1);
-			aleatorio2 = Helpers.intAleatorio(0, seqAtualFilho.size()-1);
+
+		while (aleatorio1 == aleatorio2) {
+			aleatorio1 = Helpers.intAleatorio(0, seqAtualFilho.size() - 1);
+			aleatorio2 = Helpers.intAleatorio(0, seqAtualFilho.size() - 1);
 		}
-		
-		/*Troca as cidades de posicao*/
+
+		/* Troca as cidades de posicao */
 		int cidade1 = seqAtualFilho.get(aleatorio1);
 		int cidade2 = seqAtualFilho.get(aleatorio2);
 
@@ -32,12 +35,38 @@ public class Mutacao {
 			} else {
 				seqNovaFilho.add(seqAtualFilho.get(i));
 			}
-			
+
 		}
-		
+
 		filho.setGenotipo(seqNovaFilho);
 		filho.setFi(ProcessaCidades.calculaPercurso(mapaCidades, seqNovaFilho));
-		
+
 		return filho;
+	}
+
+	public static Cromossomo mutacaoPorPosicao(Cromossomo filho,
+			HashMap<Integer, double[]> mapaCidades, int posicaoAlelo) {
+		ArrayList<Integer> seqAtualFilho = filho.getGenotipo();
+		ArrayList<Integer> seqNovaFilho = filho.getGenotipo();
+
+		int aleatorio = -1;
+		aleatorio = Helpers.intAleatorio(0, seqAtualFilho.size() - 1);
+		int valor = -1;
+
+		/* Joga cidade para posicao 'aleatorio' */
+
+		valor = seqAtualFilho.get(posicaoAlelo);
+
+		for (int i = posicaoAlelo + 1; i < aleatorio + 1; i++) {
+			seqNovaFilho.add(i, seqAtualFilho.get(i + 1));
+		}
+
+		seqNovaFilho.add(aleatorio, valor);
+
+		filho.setGenotipo(seqNovaFilho);
+		filho.setFi(ProcessaCidades.calculaPercurso(mapaCidades, seqNovaFilho));
+
+		return filho;
+
 	}
 }
